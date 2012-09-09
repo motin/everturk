5b60c06847d013dda234eb0f2a4c5c8b6e130111
Fredrik Wollsén <fredrik@neam.se>
2012-09-09 01:45:25 -0700

    Split evernote/connect code into C and V parts

--- yiiapp/protected/controllers/EvernoteController.php
+++ yiiapp/protected/controllers/EvernoteController.php
@@ -5,7 +5,61 @@ class EvernoteController extends Controller
 
 	public function actionConnect()
 	{
-		$this->render('connect');
+
+		// Include our OAuth functions
+		require_once('protected/extensions/evernote-sdk-php/sample/oauth/functions.php');
+
+		// Use a session to keep track of temporary credentials, etc
+		session_start();
+
+		// Status variables
+		global $lastError, $currentStatus;
+		$lastError = null;
+		$currentStatus = null;
+		$return = null;
+
+		//var_dump($_SESSION);
+		// Request dispatching. If a function fails, $lastError will be updated.
+		if (isset($_GET['action']))
+		{
+			$action = $_GET['action'];
+			if ($action == 'callback')
+			{
+				if (handleCallback())
+				{
+					if (getTokenCredentials())
+					{
+						$return = listNotebooks();
+					} else
+					{
+						//var_dump(__LINE__);
+					}
+				}
+			} else if ($action == 'authorize')
+			{
+				if (getTemporaryCredentials('/evernote/connect'))
+				{
+					// We obtained temporary credentials, now redirect the user to evernote.com to authorize access
+					header('Location: ' . getAuthorizationUrl('/evernote/connect'));
+					exit();
+				}
+			} else if ($action == 'reset')
+			{
+				resetSession();
+			}
+		} else
+		{
+			// Verify access by attempting to list the user's notebooks. If it doesn't work, we need to authenticate...
+			try {
+				$return = listNotebooks();
+			} catch (Exception $e) {
+				unset($lastError);
+			}
+		}
+
+		//var_dump(__LINE__, $return, $lastError, $GLOBALS['lastError'], $currentStatus);
+
+		$this->render('connect', compact("return", "lastError", "currentStatus"));
 	}
 
 }
--- yiiapp/protected/views/evernote/connect.php
+++ yiiapp/protected/views/evernote/connect.php
@@ -25,61 +25,6 @@ function time_diff_readable($secs)
 	return join(' ', $ret);
 }
 
-// Include our OAuth functions
-require_once('protected/extensions/evernote-sdk-php/sample/oauth/functions.php');
-
-// Use a session to keep track of temporary credentials, etc
-session_start();
-
-// Status variables
-global $lastError, $currentStatus;
-$lastError = null;
-$currentStatus = null;
-$return = null;
-
-//var_dump($_SESSION);
-// Request dispatching. If a function fails, $lastError will be updated.
-if (isset($_GET['action']))
-{
-	$action = $_GET['action'];
-	if ($action == 'callback')
-	{
-		if (handleCallback())
-		{
-			if (getTokenCredentials())
-			{
-				$return = listNotebooks();
-			} else
-			{
-				//var_dump(__LINE__);
-			}
-		}
-	} else if ($action == 'authorize')
-	{
-		if (getTemporaryCredentials('/evernote/connect'))
-		{
-			// We obtained temporary credentials, now redirect the user to evernote.com to authorize access
-			header('Location: ' . getAuthorizationUrl('/evernote/connect'));
-			exit();
-		}
-	} else if ($action == 'reset')
-	{
-		resetSession();
-	}
-} else
-{
-	// Verify access by attempting to list the user's notebooks. If it doesn't work, we need to authenticate...
-	try {
-		$return = listNotebooks();
-	} catch (Exception $e) {
-		unset($lastError);
-	}
-}
-
-//var_dump(__LINE__, $return, $lastError, $GLOBALS['lastError'], $currentStatus);
-?>
-
-<?php
 if (isset($lastError))
 {
 	?>
@@ -114,7 +59,7 @@ if (isset($lastError))
 			{
 				?>
 				<li><?php print $notebook; ?></li>
-		<?php } ?>
+			<?php } ?>
 		</ul>
 
 	<?php } // if (isset($_SESSION['notebooks']))        ?>
