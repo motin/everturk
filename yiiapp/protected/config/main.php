eafa9948f61afaf3827a2ebc277a8f4322e7849f
Fredrik Wollsén <fredrik@neam.se>
2012-09-08 22:14:20 -0700

    Using secrets.php + Added secrets.php.dist that shows how the secrets.php should be constructed

--- yiiapp/protected/config/main.php
+++ yiiapp/protected/config/main.php
@@ -1,5 +1,7 @@
 <?php
 
+require('secrets.php');
+
 // uncomment the following to define a path alias
 // Yii::setPathOfAlias('local','path/to/local-folder');
 // This is the main Web application configuration. Any writable
