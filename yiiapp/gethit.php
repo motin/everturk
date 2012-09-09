c3f4ca40c726d0f048dc7cb37be57a1d22716f96
Christian Puricelli <christianpuricelli@consultingmedia.it>
2012-09-09 01:12:44 -0700

    Marking finished HITs as processed in the database

--- yiiapp/gethit.php
+++ yiiapp/gethit.php
@@ -3,6 +3,8 @@
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 require_once('protected/extensions/turk50/Turk50.php');
 require_once('protected/config/secrets.php');
+$db = mysql_connect(YII_DB_HOST, YII_DB_USER, YII_DB_PASSWORD);
+mysql_select_db(YII_DB_NAME, $db);
 $mturk = new Turk50(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, array("sandbox" => FALSE));
 
 $Request = array(
@@ -20,15 +22,40 @@ foreach ($array as $hit)
 	);
 
 	$response2 = $mturk->GetAssignmentsForHIT($Request2);
-	$response3 = $mturk->GetHIT($Request2);
-	echo "<br>Q:";
-	echo $response3->HIT->Question;
-	echo "<br>A:";
-	echo $response2->GetAssignmentsForHITResult->Assignment->Answer;
-//echo "<br>r2";
-//print_r($response2); 	
-//echo "<br>r3";
-//print_r($response3); 
+	if (isset($response2->GetAssignmentsForHITResult->Assignment->Answer) && $response2->GetAssignmentsForHITResult->Assignment->Answer != "")
+	{
+		$sql = sprintf("SELECT 1 FROM `finished_assignment`  WHERE `hitid` = '%s'", mysql_real_escape_string($hit->HITId));
+		if ($result = mysql_query($sql))
+		{
+			if (mysql_num_rows($result) == 0)
+			{
+				echo "not yet processed";
+				$sql2 = sprintf("INSERT INTO `finished_assignment`  (`hitid`) values ('%s')", mysql_real_escape_string($hit->HITId));
+				if (mysql_query($sql2))
+				{
+					echo "success";
+				} else
+				{
+					echo "error $sql2 " . mysql_error();
+				}
+				$response3 = $mturk->GetHIT($Request2);
+				
+				//Create Evernote note here with Q: $response3->HIT->Question and A: $response2->GetAssignmentsForHITResult->Assignment->Answer
+
+				echo "Q:" . $response3->HIT->Question;
+				echo "A:" . $response2->GetAssignmentsForHITResult->Assignment->Answer;
+			} else
+			{
+				echo "already processed";
+			}
+		} else
+		{
+			echo "error $sql" . mysql_error();
+		}
+	} else
+	{
+		echo "no anwser";
+	}
 }
 echo "test";
 echo "</pre>";
